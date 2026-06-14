fc190cf8862c2e1d58ca61fb7caa86ce8282358d
maihan88 <hanvani0808@gmail.com>
2026-06-14 15:04:17 +0700

    add new ultils

---
+++ app/src/main/java/com/example/sencare/utils/CloudinaryUtil.java
@@ -0,0 +1,12 @@
+package com.example.sencare.utils;
+
+import android.content.Context;
+
+import com.cloudinary.android.MediaManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloudinaryUtil {
+    // Dùng sau cho upload ảnh lên Cloudinary
+}
